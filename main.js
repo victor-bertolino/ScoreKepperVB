d92c68b0740d63552704839e552b4d196e775365
Victor <victorbertolino.pro@gmail.com>
2024-02-28 09:25:51 -0300

    points subtraction feature

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const t1 = {
     score: 0,
-    button: document.querySelector('#t1Btn'),
+    plusButton: document.querySelector('#t1PlusBtn'),
+    minusButton: document.querySelector('#t1MinusBtn'),
     display: document.querySelector('#t1Display')
 }
 
 const t2 = {
     score: 0,
-    button: document.querySelector('#t2Btn'),
+    plusButton: document.querySelector('#t2PlusBtn'),
+    minusButton: document.querySelector('#t2MinusBtn'),
     display: document.querySelector('#t2Display')
 }
 
@@ -21,11 +23,11 @@ playToBtn.addEventListener('change', () => {
     reset()
 })
 
-function updateScore(team, opponent) {
+function addScore(team, opponent) {
     if (isGameOver === false) {
         team.score++
         team.display.textContent = team.score
-        if ((team.score === (winningScore - 1)) && (opponent.score === (winningScore - 1))) {
+        if ((team.score === (winningScore - 1)) && (team.score === opponent.score)) {
             winningScore++
         }
         if (team.score === winningScore) {
@@ -34,12 +36,30 @@ function updateScore(team, opponent) {
     }
 }
 
-t1.button.addEventListener('click', () => {
-    updateScore(t1, t2)
+t1.plusButton.addEventListener('click', () => {
+    addScore(t1, t2)
 })
 
-t2.button.addEventListener('click', () => {
-    updateScore(t2, t1)
+t2.plusButton.addEventListener('click', () => {
+    addScore(t2, t1)
+})
+
+function removeScore(team, opponent) {
+    if ((isGameOver === false) && (team.score !== 0)) {
+        if ((team.score === (winningScore - 2)) && (team.score === opponent.score)) {
+            winningScore--
+        }
+        team.score--
+        team.display.textContent = team.score
+    }
+}
+
+t1.minusButton.addEventListener('click', () => {
+    removeScore(t1, t2)
+})
+
+t2.minusButton.addEventListener('click', () => {
+    removeScore(t2, t1)
 })
 
 function reset() {
